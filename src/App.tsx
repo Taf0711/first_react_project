3c21a69d06fa13ca9af925d4c7d8f6bb00dc1746
Taf <haquetafseer@gmail.com>
2023-03-17 22:19:52 -0400

    Work from last night

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React, {FC, useState} from "react";
-import './App.css'
+import React, { FC, ChangeEvent, useState } from "react";
+import "./App.css";
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todo, setTodoList] = useState([]);
 
-  const [task, setTask] = useState<string>("")
-  const [deadline, setDeadline] = useState<string>("")
-  const [task, setTask] = useState<string>("")
-    return (
-      <div className='App'> 
-        <div className='header'>
-          <div className="inputContainer">
-          <input type="text" placeholder="Task..." />
-          <input type="number" placeholder="Deadline (in Days)..." />
-          </div>
-          <button>Add Task</button>
-        </div>
-        <div className='todolist'></div>
-      </div>
-    );
+  const handleChange = (event: ChangeEvent<HTMLInputElement> ) => {
+    setTask(event.target.value)
   };
-  export default App;
-  
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+          <input type="text" placeholder="Task..." onChange={handleChange} />
+          <input type="number" placeholder="Deadline (in Days)..." onChange={handleChange} />
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className="todolist"></div>
+    </div>
+  );
+};
+export default App;
